d01e78a72a6d43f7c9af9116572e716f8c4667cf
Sam Robbins <samrobbinsgb@gmail.com>
2021-06-04 13:11:42 +0100

    ✨ Use next-sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+	siteUrl: "https://notes.samrobbins.uk",
+	generateRobotsTxt: true,
+};
--- sitemap_generator.js
+++
@@ -1,9 +0,0 @@
-const sitemap = require("nextjs-sitemap-generator");
-
-sitemap({
-	baseUrl: "https://csnotes.me",
-	pagesDirectory: __dirname + "/notes",
-	targetDirectory: "public/",
-	ignoredExtensions: ["js", "map"],
-	ignoredPaths: ["[fallback]"],
-});
